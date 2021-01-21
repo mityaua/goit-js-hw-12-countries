74d4f3a6ce68ed41853fa6dcecc67ac17578fff0
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-01-21 12:43:53 +0200

    Add data.status, fix styles

--- src/js/update-countries.js
+++ src/js/update-countries.js
@@ -1,7 +1,7 @@
 // Импорт шаблона для списка стран
 import countriesTpl from '../templates/countries-list.hbs';
 import countryTpl from '../templates/country-item.hbs';
-import alert from './notify'
+import alert from './notify';
 
 // DOM
 const listContainerRef = document.querySelector('.markup-container');
@@ -10,6 +10,16 @@ const listContainerRef = document.querySelector('.markup-container');
 function updateCountriesList(data) {
   const markupAll = countriesTpl(data);
   const markupOne = countryTpl(data);
+
+  if (data.status === 404) {
+    alert({
+      type: 'notice',
+      text: 'Nothing found ☹',
+      delay: 2000,
+      width: '300px',
+      maxTextHeight: null,
+    });
+  }
 
   if (data.length === 1) {
     listContainerRef.insertAdjacentHTML('beforeend', markupOne);
@@ -20,7 +30,7 @@ function updateCountriesList(data) {
     alert({
       type: 'error',
       text: 'Too many matches found. Please enter a more specific query!',
-      delay: 3000,
+      delay: 2000,
       maxTextHeight: null,
     });
     return;
