3ba9a37a09e704b2bd4fcc8c59905b239b3c7734
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-02-07 12:29:12 +0200

    Fix takeSearchResults

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
-// Импорты
-import { form, inputRef, resetRef, markupContainerRef } from './js/refs';
+// Scripts
+import './js/search'
 
-import debounce from 'lodash/debounce';
+// Styles
 import './sass/styles.scss';
-import fetchCountries from './js/fetchCountries';
-import updateCountriesList from './js/update-countries';
 
-// Слушатели событий на инпутах
-inputRef.addEventListener('input', debounce(searchCountries, 500));
-resetRef.addEventListener('click', clearContainer);
-form.addEventListener('submit', event => {
-  event.preventDefault();
-});
 
-// Функция для поиска
-function searchCountries(event) {
-  // event.preventDefault();
 
-  const searchQuery = event.target.value;
-
-  clearContainer();
-
-  fetchCountries(searchQuery).then(updateCountriesList);
-
-  // markupContainerRef.addEventListener('click', takeSearchResults);
-}
-
-// Функция для подстановки результатов из выдачи (снять слушатель после отрисовки выше!)
-// function takeSearchResults(event) {
-//   inputRef.value = event.target.textContent;
-
-//   clearContainer();
-
-//   fetchCountries(inputRef.value).then(updateCountriesList);
-
-//   markupContainerRef.removeEventListener('click', takeSearchResults);
-// }
-
-// Функция для очистки выдачи
-function clearContainer() {
-  markupContainerRef.innerHTML = '';
-}
---
+++ src/js/search.js
@@ -0,0 +1,42 @@
+import { form, inputRef, resetRef, markupContainerRef } from './refs';
+import debounce from 'lodash/debounce';
+import fetchCountries from './fetchCountries';
+import updateCountriesList from './update-countries';
+
+// Слушатели событий на инпутах
+inputRef.addEventListener('input', debounce(searchCountries, 500));
+resetRef.addEventListener('click', clearContainer);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+// Функция для поиска
+function searchCountries(event) {
+  const searchQuery = event.target.value;
+
+  clearContainer();
+
+  fetchCountries(searchQuery).then(updateCountriesList);
+
+  markupContainerRef.addEventListener('click', takeSearchResults);
+}
+
+// Функция для подстановки результатов из выдачи
+function takeSearchResults(event) {
+  if (!event.target.classList.contains('js-list-title')) {
+    return;
+  }
+
+  inputRef.value = event.target.textContent;
+
+  clearContainer();
+
+  fetchCountries(inputRef.value).then(updateCountriesList);
+
+  markupContainerRef.removeEventListener('click', takeSearchResults);
+}
+
+// Функция для очистки выдачи
+function clearContainer() {
+  markupContainerRef.innerHTML = '';
+}
